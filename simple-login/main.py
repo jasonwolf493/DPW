df34ff2eadd65ff41503f183a7abfd1504eeeebb
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-11-06 05:28:05 -0500

    continued simple login build
    continuing to build the simple login with the follow along video

--- simple-login/main.py
+++ simple-login/main.py
@@ -9,11 +9,25 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything
-        self.response.write('Hello world!')
-        #code goes here
-
-    def additional_functions(self):
-        pass
+        page = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Login</title>
+    </head>
+    <body>
+        <form method="GET">
+            <label>Name:</label><input type="text" name="user"/>
+            <label>Email:</label><input type="text" name="email"/>
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+</html>
+        '''
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+        self.response.write(page) #this prints info
         #code goes here
 
 
