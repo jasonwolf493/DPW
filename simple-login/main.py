8049db4903d396a2acf150f04bb3d9a3efb497e4
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-11-06 05:34:42 -0500

    else statement added
    if there is nothing typed in fields it will not break the page

--- simple-login/main.py
+++ simple-login/main.py
@@ -9,25 +9,27 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything
-        page = '''
+        page_head = '''
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>Simple Login</title>
     </head>
-    <body>
-        <form method="GET">
+    <body>'''
+
+        page_body = '''<form method="GET">
             <label>Name:</label><input type="text" name="user"/>
             <label>Email:</label><input type="text" name="email"/>
-            <input type="submit" value="Submit" />
-        </form>
+            <input type="submit" value="Submit" />'''
+        page_close = '''</form>
     </body>
 </html>
         '''
         if self.request.GET:
             user = self.request.GET['user']
             email = self.request.GET['email']
-        self.response.write(page) #this prints info
+        else:
+            self.response.write(page_head + page_body + page_close) #this prints info
         #code goes here
 
 
