2fb4c89598eac47081a4e7a395268a5e9d739cc3
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-11-05 22:36:38 -0500

    Data Object
    First data object complete with character names

---
+++ data-objects/main.py
@@ -0,0 +1,43 @@
+"""
+Jason Wolf
+11/5/2014
+DPW
+Data-Objects
+"""
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        luke = Character()
+        luke.name = "Luke Skywalker"
+        luke.profession = "Jedi Knight"
+        luke.age = 26
+        luke.home_planet = "Tattooine"
+
+        leia = Character()
+        leia.name = "Princess Leia"
+        leia.profession = "Princess"
+        leia.age = luke.age
+        leia.home_planet = "Alderan"
+
+        yoda = Character()
+        yoda.name = "Master Yoda"
+        yoda.profession = "Jedi Knight"
+        yoda.age = 762
+        yoda.home_planet = "Dagobah"
+
+        chars = [luke, leia, yoda]
+        print chars[1].profession
+
+
+class Character(object):
+    def __init__(self): #constructor
+        self.name = ""
+        self.profession = ""
+        self.age = 0
+        self.home_planet = ""
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
