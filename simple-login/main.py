e78e566088a3a8b0918649fb1edf9c2978475857
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-11-06 23:11:49 -0500

    fixed css, added classes, select added, very sleek form, very minimalistic and clean UI

--- simple-login/main.py
+++ simple-login/main.py
@@ -18,18 +18,52 @@ class MainHandler(webapp2.RequestHandler): #declaring a class
     </head>
     <body>'''
 
-        page_body = '''<form method="GET">
-            <label>Name:</label><input type="text" name="user"/>
-            <label>Email:</label><input type="text" name="email"/>
-            <input type="submit" value="Submit" />'''
-        page_close = '''</form>
+
+        #<label>First Name:</label>
+        #<label>Last Name:</label>
+        #<label>Address:</label>
+        page_shipped = '''
+        <div class="title">
+            <h1>Christmas Shipper</h1>
+        </div>
+        <div class="info">
+
+        '''
+        page_body = '''
+        <div class="title">
+            <h1>Christmas Shipper</h1>
+        </div>
+        <div class="center">
+        <form method="GET">
+            <input type="text" placeholder="First Name" name="first"/><br>
+            <input type="text" placeholder="Last Name" name="last"/><br>
+            <input class="address" type="text" placeholder="Address" name="address"/><br>
+            <label>Shipping:</label><br>
+            <select name="shipping">
+                <option value="ground shipping">Ground</option>
+                <option value="two day shipping">2 Day</option>
+                <option value="overnight shipping">Overnight</option>
+            </select>
+            <input class="submit" type="submit" value="Submit" />'''
+        page_close = '''</form></div>
     </body>
 </html>
         '''
         if self.request.GET:
-            user = self.request.GET['user']
-            email = self.request.GET['email']
-            self.response.write(page_head + user + " " + email +page_body + page_close)
+            first_name = self.request.GET['first']
+            last_name = self.request.GET['last']
+            address = self.request.GET['address']
+            shipping = self.request.GET['shipping']
+            if shipping == "ground shipping":
+                days = "5"
+            if shipping == "two day shipping":
+                days = "2"
+            else:
+                days = "1"
+
+
+
+            self.response.write(page_head + page_shipped + first_name + " " + last_name + "'s package will arrive at " + address + " via " + shipping + " about " + days + " day after the package departs."  + page_close)
         else:
             self.response.write(page_head + page_body + page_close) #this prints info
         #code goes here
