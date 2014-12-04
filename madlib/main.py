a8d19aabdad5779ad04403b9ed03aa459a25a5bd
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-12-03 23:43:18 -0500

    commented

--- madlib/main.py
+++ madlib/main.py
@@ -9,6 +9,7 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+        #the required obj are below they will be used in the Character class to gen_story
         char1 = Character()
         char1.name = "Jack"
         char1.profession = "peasant"
@@ -27,8 +28,11 @@ class MainHandler(webapp2.RequestHandler):
         char3.race = "human"
         char3.age = 21
 
+        #create an array to store all of the chars in.
         chars = [char1, char2, char3]
+        #make an array so we can loop through the above array.
         x = 0
+        #use this for loop with the above array and x var so we can loop through the array and send each person to Character.gen_story
         for people in chars:
             self.response.write(Character.gen_story(people, x))
             x += 1
@@ -38,6 +42,7 @@ class MainHandler(webapp2.RequestHandler):
 
 class Character(object):
     def __init__(self):
+        #what char traits we will be using
         self.name = ""
         self.profession = ""
         self.age = 0
@@ -45,17 +50,22 @@ class Character(object):
         self.race = ""
         print self.name
 
+    #we will use this to gen the story
     def gen_story(self, x):
 
+        #use text to hold the story
         text = self.name + " he has spent most of his life as a " + self.profession + ", he is " + str(self.age) + "yrs old in human years and he is a " + self.race
+        # if the char is an elf we will reverse the math done earlier so we can see their actual age
         if self.race == "elf":
             text = text + " making him " + str(self.age / 1.5)
         elif self.race == "dwarf":
             text = text + " making him " + str(self.age * 2)
 
+        #yet another if conditional, if its our first char we prepend some text
         if x == 0:
             text = "The journey begins with three brave souls, " + text
             return text + ". "
+        #else return it normally
         else:
             return text + ". "
 
