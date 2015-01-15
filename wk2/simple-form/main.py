030eacc37f8c518d129a22592a41e0088384b31b
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2015-01-15 05:44:45 -0500

    Everything seems to function correctly.

--- wk2/simple-form/main.py
+++ wk2/simple-form/main.py
@@ -1,11 +1,54 @@
 import webapp2
-from pages import Page
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
-        p = Page()
-        self.response.write(p.build_page())
+
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Shippr</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+        """
+
+        self.body = """
+            <h1>Shippr</h1>
+            Fill your shipping info below and shippr will handle the rest!
+        """
+        self.form = """
+            <form method="GET" action="">
+                <input type="text" placeholder="John" name="first_name" required/>
+                <input type="text" placeholder="Doe" name="last_name" required/><br>
+                <input type="text" placeholder="123 Main St." name="address" required/>
+                <select name="state">
+                    <option value="Connecticut">Connecticut</option>
+                    <option value="Massachusetts">Massachusetts</option>
+                    <option value="Ohio">Ohio</option>
+                    <option value="Pennsylvania">Pennsylvania</option>
+                </select>
+                <br><input type="radio" name="agree" value="true">I agreed to the shipping terms and conditions
+                <br><input type="submit" value="Submit" />
+            </form>
+        """
+        self.close = """
+        </div>
+    </body>
+</html>
+        """
+
+        if self.request.GET:
+            first_name = self.request.GET['first_name']
+            last_name = self.request.GET['last_name']
+            address = self.request.GET['address']
+            state = self.request.GET['state']
+            self.body = "<h1>Thanks!</h1><h2>" + first_name + " " + last_name + " our team will jump right on it.</h2>"
+            self.response.write(self.head + self.body + "<br> First Name: " + first_name + "<br><br>Last Name: " + last_name + "<br><br>Address: " + address + "<br><br>State: " + state + "<br><br>Your package will leave our facility momentarily" + self.close)
+        else:
+            self.response.write(self.head + self.body + self.form + self.close)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
