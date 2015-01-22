e4b57df8030d9d16b88dca8798e057f9e6ab3d1c
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2015-01-22 15:32:08 -0500

    Added forgotten comments
    added missing comments oopps

--- wk2/Lab 2 Server Side Form/main.py
+++ wk2/Lab 2 Server Side Form/main.py
@@ -4,6 +4,7 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+        #This is the head for the HTML using doc string
         self.head = """
 <!DOCTYPE HTML>
 <html>
@@ -15,10 +16,12 @@ class MainHandler(webapp2.RequestHandler):
         <div id="container">
         """
 
+        #this is the body for the HTML also in a doc string
         self.body = """
             <h1>Shippr</h1>
             Fill your shipping info below and shippr will handle the rest!
         """
+        #this is the form that the users will enter their info into in HTML
         self.form = """
             <form method="GET" action="">
                 <input id="textinput" type="text" placeholder="John" name="first_name" required/>
@@ -35,12 +38,13 @@ class MainHandler(webapp2.RequestHandler):
                 <br><input id="submit" type="submit" value="Submit" />
             </form>
         """
+        # all the closing tag for the HMTL
         self.close = """
         </div>
     </body>
 </html>
         """
-
+        #if the program gets something submitted then we will change the body to show some of the results
         if self.request.GET:
             first_name = self.request.GET['first_name']
             last_name = self.request.GET['last_name']
@@ -49,6 +53,7 @@ class MainHandler(webapp2.RequestHandler):
             self.body = "<h1>Thanks!</h1><h2>" + first_name + " " + last_name + " our team will jump right on it.</h2>"
             self.response.write(self.head + self.body + " <br>First Name: " + first_name + "<br>Last Name: " + last_name + "<br>Address: " + address + "<br>State: " + state + "<br><br>Your package will leave our facility momentarily <br>" + "<a href='/'>Ship something else!</a>" + self.close)
         else:
+            #otherwise we are just going to print it as it is.
             self.response.write(self.head + self.body + self.form + self.close)
 
 app = webapp2.WSGIApplication([
